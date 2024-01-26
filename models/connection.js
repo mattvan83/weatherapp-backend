7c805f93e59fda37c45087622238c05e2f195115
Matthieu Vanhoutte <matthieuvanhoutte@gmail.com>
2024-01-26 19:36:50 +0100

    Create .env for weatherapp backend deployment on Vercel

--- models/connection.js
+++ models/connection.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const connectionString =
-  "mongodb+srv://admin:lEi8gAghhS0hci5e@cluster0.3lphr5o.mongodb.net/weatherapp";
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose
   .connect(connectionString, { connectTimeoutMS: 2000 })
